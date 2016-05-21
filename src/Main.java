9b0ff1accb91b5cb848eef6ad03b6d539369069b
Adi <adelina_yordanova11@abv.bg>
2016-05-21 13:54:20 +0300

    HomeWork02_10.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 16;
+		System.out.print("a ");
+		System.out.println(a);
+		int b = 9;
+		System.out.print("b ");
+		System.out.println(b);
+		int S = ((a * b) / 2);
+		System.out.println("S =(a*b)/2: " + S);
+	}
+}
